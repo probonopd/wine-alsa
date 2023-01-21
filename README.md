6218d187ec6721503245ad0b850a138817fb03c4
probonopd <probonopd@users.noreply.github.com>
2023-01-21 17:00:56 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# wine-alsa [![Build Status](https://api.cirrus-ci.com/github/helloSystem/wine-alsa.svg)](https://cirrus-ci.com/github/helloSystem/wine-alsa)
+
+[Built packages](https://api.cirrus-ci.com/v1/artifact/github/helloSystem/wine-alsa/pkg/binary/FreeBSD:13:amd64/index.html)
+
+```sh
+sudo su
+
+cat > /usr/local/etc/pkg/repos/wine-alsa.conf <<\EOF
+wine-alsa: {
+        url: "pkg+https://api.cirrus-ci.com/v1/artifact/github/helloSystem/wine-alsa/pkg/binary/${ABI}",
+        mirror_type: "srv",
+        enabled: yes,
+        priority: 100
+}
+EOF
+exit
+
+sudo pkg install wine-alsa
+```
---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+build_package()
+{
+  PORT=$1
+  ( cd "${PORT}" && make build-depends-list | cut -c 12- | xargs pkg install -y ) 
+  make -C "${PORT}" package
+}
+
+mount -t unionfs $(readlink -f .) /usr/ports
+HERE="${PWD}"
+cd /usr/ports
+
+# wine-alsa
+build_package emulators/wine-alsa
+
+cd "${HERE}"
+umount /usr/ports
+
+# FreeBSD repository
+ABI=$(pkg config abi) # E.g., FreeBSD:13:amd64
+mkdir -p "${ABI}"
+find . -name '*.pkg' -exec mv {} "${ABI}/" \;
+pkg repo "${ABI}/"
+# index.html for the FreeBSD repository
+cd "${ABI}/"
+echo "<html><ul>" > index.html
+find . -depth 1 -exec echo '<li><a href="{}" download>{}</a></li>' \; | sed -e 's|\./||g' >> index.html
+echo "</ul></html>" >> index.html
+cd -
